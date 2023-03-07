6218f2ddfb2c805f4a4258e56b64a11ed23d9c63
Vince <v.coccina@orange.fr>
2023-03-07 18:22:46 +0100

    vercel

--- next.config.js
+++ next.config.js
@@ -24,13 +24,6 @@ module.exports = {
   images:{
     domains:["res.cloudinary.com"]
   },
-  env: {
-    MONGODB_URI:
-      "mongodb+srv://vince75019:vince75019@food.v6wdjjn.mongodb.net/foodOrder?retryWrites=true&w=majority",
-    ADMIN_USERNAME: "admin",
-    ADMIN_PASSWORD: "vince",
-    TOKEN: "SWdw4CV||663Z{p3|ZXtP%0k6Ejj;F",
-  },
 };
 
 
