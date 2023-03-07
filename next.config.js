260484244be6ed7d2fb1b563b747374e50753675
Vince <v.coccina@orange.fr>
2023-03-07 17:37:03 +0100

    deploy

--- next.config.js
+++ next.config.js
@@ -1,31 +1,37 @@
-const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+}
+module.exports = nextConfig
 
-module.exports = (phase) => {
-  if ((phase = PHASE_DEVELOPMENT_SERVER)) {
-    return {
-      images: {
-        domains: ["res.cloudinary.com"],
-      },
-      env: {
-        MONGODB_URI:
-          "mongodb+srv://vince75019:vince75019@food.v6wdjjn.mongodb.net/foodOrder?retryWrites=true&w=majority",
-        ADMIN_USERNAME: "admin",
-        ADMIN_PASSWORD: "vince",
-        TOKEN: "SWdw4CV||663Z{p3|ZXtP%0k6Ejj;F",
-      },
-    };
-  }
 
-  return {
-    images: {
-      domains: ["res.cloudinary.com"],
-    },
-    env: {
-      MONGODB_URI:
-        "mongodb+srv://vince75019:vince75019@food.v6wdjjn.mongodb.net/foodOrder?retryWrites=true&w=majority",
-      ADMIN_USERNAME: "admin",
-      ADMIN_PASSWORD: "vince",
-      TOKEN: "SWdw4CV||663Z{p3|ZXtP%0k6Ejj;F",
-    },
-  };
+module.exports = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.(woff|woff2|eot|ttf|otf)$/i,
+      use: {
+        loader: 'file-loader',
+        options: {
+          publicPath: '/_next/static/fonts/',
+          outputPath: 'static/fonts/',
+          name: '[name].[ext]',
+          esModule: false,
+        },
+      },
+    });
+    return config;
+  },
+  images:{
+    domains:["res.cloudinary.com"]
+  },
+  env: {
+    MONGODB_URI:
+      "mongodb+srv://vince75019:vince75019@food.v6wdjjn.mongodb.net/foodOrder?retryWrites=true&w=majority",
+    ADMIN_USERNAME: "admin",
+    ADMIN_PASSWORD: "vince",
+    TOKEN: "SWdw4CV||663Z{p3|ZXtP%0k6Ejj;F",
+  },
 };
+
+
+
